53a858c4feb00afcb0baea0f88baef1e7b80b827
Thomas Klemm <github@tklemm.eu>
2012-11-03 15:36:02 +0100

    Chart rerenders on timeframe selection change

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,15 @@
 // Create App
-var TS = Em.Application.create();
+var TS = Em.Application.create({
+  // ready: function() {
+  //   // Set current time once per second
+  //   setInterval(function() {TS.stats.setCurrentTime()}, 1000);
+  // }
+});
+
+// Prototype Extensions
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
 
 /**************************
 * Models
@@ -28,16 +38,22 @@ TS.Tweet = Em.Object.extend({
 // to determine which timeframe for graphing is selected
 var selected_chart = null;
 var charts = [
-    Em.Object.create({id: 1, label: "1 Hour"}),
-    Em.Object.create({id: 2, label: "10 Minutes"}),
-    selected_chart = Em.Object.create({id: 3, label: "1 Minute"}),
-    Em.Object.create({id: 4, label: "10 Seconds"})
+  Em.Object.create({id: 1, label: "1 Hour",     format: "YYYY-M-DD HH:00",    last_digit_zero: false}),
+  Em.Object.create({id: 2, label: "10 Minutes", format: "YYYY-M-DD HH:mm",    last_digit_zero: true}),
+  selected_chart =
+  Em.Object.create({id: 3, label: "1 Minute",   format: "YYYY-M-DD HH:mm",    last_digit_zero: false}),
+  Em.Object.create({id: 4, label: "10 Seconds", format: "YYYY-M-DD HH:mm:ss", last_digit_zero: true})
 ]
 
 // Charts Controller
 TS.chartsC = Em.Object.create({
   selectedChart: selected_chart,
-  content: charts
+  content: charts,
+  // Render graph on selection change
+  selectedChartChanged: function() {
+    // Render graph
+    draw_graph();
+  }.observes('selectedChart')
 });
 
 // Stats Object
@@ -47,47 +63,82 @@ TS.stats = Em.Object.create({
   date_end: null,
 
   // Averages per timeframe
-  avg_hours:   0.0,
-  avg_minutes: 0.0,
-  avg_seconds: 0.0,
+  avg_hours:      0,
+  avg_10_minutes: 0,
+  avg_minutes:    0,
+  avg_10_seconds: 0,
 
-  // Maxiumums per timeframe
-  max_hours:   0,
-  max_minutes: 0,
-  max_seconds: 0,
+  // Maxima per timeframe
+  max_hours:      0,
+  max_10_minutes: 0,
+  max_minutes:    0,
+  max_10_seconds: 0,
+
+  // Current time
+  // for initial graph
+  current_time: 'now',
+
+  // Set current time
+  // runs every second
+  // setCurrentTime: function() {
+  //   this.set('current_time', moment().format("YYYY-M-DD HH:mm:ss"));
+  // },
 
   // Reset all stats
   // e.g. when new search is being submitted
   reset: function() {
+    // Date Range of tweets
     this.set('date_begin', null);
     this.set('date_end', null);
 
+    // Maxima per timeframe
     this.set('max_hours', 0);
+    this.set('max_10_minutes', 0);
     this.set('max_minutes', 0);
-    this.set('max_seconds', 0);
+    this.set('max_10_seconds', 0);
 
+    // Averages per timeframe
     this.set('avg_hours', 0);
+    this.set('avg_10_minutes', 0);
     this.set('avg_minutes', 0);
-    this.set('avg_seconds', 0);
+    this.set('avg_10_seconds', 0);
   },
 
-  // Computed Stats
-  // Relative Timestamps for begin and end of tweets' data range
+  // Set current stats from tweet in processing
+  // e.g. time range of retrieved tweets
+  tweetStats: function(tweet) {
+      // Initial store of first tweet
+      if (!TS.stats.date_begin) TS.stats.set('date_begin', tweet.created_at);
+      if (!TS.stats.date_end)   TS.stats.set('date_end', tweet.created_at);
+
+      // Current stats
+      if (tweet.created_at < TS.stats.date_begin) TS.stats.set('date_begin', tweet.created_at);
+      if (tweet.created_at > TS.stats.date_end)   TS.stats.set('date_end', tweet.created_at);
+  },
+
+  // Computed Time Range of Tweets
+  // Relative time ('2 minutes ago')
   oldest: function() {
     var time = this.get('date_begin');
     time = time ? moment(time).fromNow() : '-';
-    return time.capitalizeFirstLetter();
-  }.property('date_begin').cacheable(),
-
-  oldest_time: function() {
-    time = this.get('date_begin');
-    return time ? moment(time).format('HH:mm:ss') : '-';
+    return time.capitalize();
   }.property('date_begin').cacheable(),
 
   newest: function() {
     var time = this.get('date_end');
     time = time ? moment(time).fromNow() : '-';
-    return time.capitalizeFirstLetter();
+    return time.capitalize();
+  }.property('date_end').cacheable(),
+
+  // Calendar time ('Today at 12:00 PM')
+  oldest_time: function() {
+    time = this.get('date_begin');
+    return time ? moment(time).calendar() : '-';
+  }.property('date_begin').cacheable(),
+
+  newest_time: function() {
+    time = this.get('date_end');
+    return time ? moment(time).calendar() : '-';
   }.property('date_end').cacheable(),
 });
 
@@ -96,8 +147,8 @@ TS.stats = Em.Object.create({
 **************************/
 
 // SearchField
-// with Autofocus
 TS.SearchField = Em.TextField.extend({
+  // Autofocus after insert
   didInsertElement: function() {
     this.$().focus();
   }
@@ -125,8 +176,11 @@ TS.tweetsC = Em.ArrayController.create({
   search: function() {
     var query = this.get('query');
 
-    // Perform search only if query is present, otherwise return
+    // Perform search only if query is present
+    // otherwise reset view and return
     if (Em.empty(query)) {
+      this.set('content', []);
+      TS.stats.reset();
       return;
     }
 
@@ -158,13 +212,13 @@ TS.tweetsC = Em.ArrayController.create({
     $.getJSON(url, function(data) {
       // Add tweets to collection
       // Returns number of tweets added
-      var tweets_added_count = self.addTweets(data.results)
+      var add_count = self.addTweets(data.results)
 
       // Exit search loop if no new tweets were added in the current run
-      if (tweets_added_count === 0) return;
+      if (add_count === 0) return;
 
       // Exit search loop if query changed
-      if (orig_query == self.get('query') return;
+      if (orig_query !== self.get('query')) return;
 
       // Build fragment for next search run containing new and lower max_id
       // of the currently oldest retrieved tweet
@@ -179,12 +233,18 @@ TS.tweetsC = Em.ArrayController.create({
   // Checks for uniqueness of tweets and tries to prevent duplicates
   // Returns the number of unique tweets that were added to the collection
   addTweets: function(tweets) {
-    var new_tweets = [];
+    // Array for new unique tweets to be added to collection
     var self = this;
+    var new_tweets = [];
 
-    for (var i = 0; i < tweets.length; i++) {
-      // Map relevant values
-      var t = tweets[i]
+    // Iterate over given raw tweets
+    var length = tweets.length;
+    for (i = 0; i < length; i++) {
+      // Single raw tweet
+      var t = tweets[i];
+
+      // Create a tweet model for each raw tweet
+      // and map respective values
       var tweet = TS.Tweet.create({
         id:         t.id,
         name:       t.from_user_name,
@@ -194,31 +254,23 @@ TS.tweetsC = Em.ArrayController.create({
         created_at: new Date(t.created_at)
       });
 
-      // Add tweet to collection unless it is already in there
-      if (this._idCache.indexOf(tweet.id) === -1) {
+      // Check for uniqueness of tweet
+      // If it is unique then...
+      //   a) add it to the new_tweets array
+      //   b) store the tweet's id in _idCache
+      if (self._idCache.indexOf(tweet.id) === -1) {
         new_tweets.push(tweet);
-        // Maybe unshift
-        this._idCache.push(tweet.id);
+        self._idCache.push(tweet.id);
 
-        // Store _minId
+        // Store _minId for next search loop run
         if (!self._minId) self.set('_minId', tweet.id);
         if (tweet.id < self._minId) self.set('_minId', tweet.id);
 
-        // Store oldest tweet date
-        if (!TS.stats.date_begin) {
-          TS.stats.set('date_begin', tweet.created_at)
-        } else if (tweet.created_at < TS.stats.date_begin) {
-          TS.stats.set('date_begin', tweet.created_at)
-        }
-
-        // Store newest tweet date
-        if (!TS.stats.date_end) {
-          TS.stats.set('date_end', tweet.created_at)
-        } else if (tweet.created_at > TS.stats.date_end) {
-          TS.stats.set('date_end', tweet.created_at)
-        }
+        // Refresh stats
+        // e.g. time range of retrieved tweets
+        TS.stats.tweetStats(tweet);
       }
-    }
+    };
 
     // Push tweets to array
     this.pushObjects(new_tweets);
@@ -229,17 +281,16 @@ TS.tweetsC = Em.ArrayController.create({
   },
 
   // Count tweets
-  // and flag function as a computed property dependent on content
-  // cacheable might already be default
+  // flagged as property dependend on content.@each
   tweetsCount: function() {
     return this.get('content').length;
   }.property('content.@each').cacheable(),
 
+  // Observer called when content array
+  // containing tweet models changes
   contentChanged: function() {
-    var content = this.get('content');
-    var new_data =  graph_data(content);
-    graph.setData(new_data);
-    $('img.lazy').lazyload();
+    // Calculate and set new graph data
+    draw_graph();
   }.observes('content.@each')
 });
 
@@ -247,99 +298,82 @@ TS.tweetsC = Em.ArrayController.create({
 * App Logic
 **************************/
 
-random_number = function() {
-  return Math.floor(Math.random()*1001)
-}
+// Draw a new morris.js graph
+draw_graph = function() {
+  // Get current tweets
+  var tweets = TS.tweetsC.get('content');
 
-var format_string_hours   = 'YYYY-M-DD HH:00';
-var format_string_minutes = 'YYYY-M-DD HH:mm';
-var format_string_seconds = 'YYYY-M-DD HH:mm:ss';
-var current_time = moment().format(format_string_minutes);
+  // Calculate Graph data from tweets array
+  var graph_data = format_tweet_data_for_morris(tweets);
 
-format_string = function(max) {
-  return format_string_hour;
-  if (max < 100) {
-    return format_string_hour;
-  } else if (max < 200) {
-
-  };
+  // Refresh graph with new data
+  graph.setData(graph_data);
 };
 
-group_tweets = function(content, timeframe) {
-  // Format string to use for time formatting
-  var format_string = format_string_minutes;
-  if (timeframe == 'hours') {
-    format_string = format_string_hours;
-  } else if (timeframe == 'seconds') {
-    format_string = format_string_seconds;
-  }
+format_tweet_data_for_morris = function(tweets) {
+  // Determine timeframe
+  var timeframe = TS.chartsC.selectedChart;
 
-  group = {};
+  // Count tweets per timeframe
+  var tweets_and_counts = tweets_count_per_time(tweets, timeframe);
 
-  content.map(function(tweet) {
-    var time = moment(tweet.created_at).format(format_string);
-    // Transform to 10 second windows
-    if (format_string == format_string_seconds) {
-      time = time.slice(0, -1) + '0'
+  // Format data for morris
+  var data = [];
+  for (time in tweets_and_counts) {
+    data.push({'time': time, 'count': tweets_and_counts[time]});
+  };
+
+  // Return sample data if there is no current data
+  // empties chart when resetting query
+  if (Em.empty(data)) return data = [{'time': TS.stats.current_time, 'count': 0}];
+
+  // Return data formatted for morris
+  return data;
+};
+
+// Group tweets by timeframe
+// Takes tweets array and a time format string
+// and groups tweets accordingly
+// Returns an object with times as properties and ...
+tweets_count_per_time  = function(tweets, timeframe) {
+  // Times with tweet count
+  var times_and_counts = {};
+
+  // Iterate over tweets
+  // to transform their timestamp to a common format,
+  // and count the tweets for each timestamp
+  var length = tweets.length;
+  for (i = 0; i < length; i++) {
+    var tweet = tweets[i];
+    var time = moment(tweet.created_at).format(timeframe.format);
+
+    // For 10 minutes and 10 seconds frame
+    // replace last digit with 0
+    if (timeframe.last_digit_zero) {
+      time = time.slice(0, -1) + '0';
     }
 
-    group[time] = (group[time] || 0) + 1;
-  });
-
-  return group;
-}
-
-max_tweets = function(group) {
-  max = 0;
-
-  for (key in group) {
-    if (group[key] > max) max = group[key];
-  };
-
-  return max;
-}
-
-graph_data = function(content) {
-  var group_hours   = group_tweets(content, 'hours');
-  var group_minutes = group_tweets(content, 'minutes');
-  var group_seconds = group_tweets(content, 'seconds');
-
-  var max_hours   = max_tweets(group_hours);
-  var max_minutes = max_tweets(group_minutes);
-  var max_seconds = max_tweets(group_seconds);
-
-  TS.stats.set('max_hours',   max_hours);
-  TS.stats.set('max_minutes', max_minutes);
-  TS.stats.set('max_seconds', max_seconds);
-
-  // Determine which group to display
-  var group = {};
-  if (content.length < 120) {
-    group = group_minutes;
-  } else if (max_hours < 170) {
-    group = group_hours;
-  } else if (max_minutes > 30) {
-    group = group_seconds;
-  } else {
-    group = group_minutes;
+    // Increment timestamp counter
+    times_and_counts[time] = (times_and_counts[time] || 0) + 1;
   }
 
-  var data = []
-  for (time in group) {
-    data.push({'time': time, 'count': group[time]});
-  };
-
-  // Return data
-  if (!Em.empty(data)) {
-    return data;
-  } else {
-    return data = [{'time': current_time, 'count': 0}]
-  }
+  // Return times and counts
+  return times_and_counts;
 };
+
+// max_tweets = function(group) {
+//   max = 0;
+
+//   for (key in group) {
+//     if (group[key] > max) max = group[key];
+//   };
+
+//   return max;
+// }
 
 var graph = Morris.Line({
   element: 'chart',
-  data: [{'time': current_time, 'count': 0}],
+  data: [{'time': TS.stats.current_time, 'count': 0}],
   xkey: 'time',
   ykeys: ['count'],
   labels: ['Tweets'],
